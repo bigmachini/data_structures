b9516fda4b8e3d5fd948cbdae9a898a7d2658aec
ewaudo <ewaudo@copiakenya.com>
2021-11-03 00:54:58 +0300

    add iterative binary search

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,5 @@
 def binary_search_recursive(_list, search_value, l, h):
-    if h-l == 1:
+    if h - l == 1:
         if _list[l] == search_value:
             return l
         elif _list[h] == search_value:
@@ -15,7 +15,31 @@ def binary_search_recursive(_list, search_value, l, h):
 
         return binary_search_recursive(_list, search_value, l, h)
 
-x = range(0,1000,7)
 
-y = binary_search_recursive(list(x),777, 0, len(x)-1)
+def binary_search_iterative(_list, search_value):
+    l = 0
+    h = len(_list)-1
+    while h - l > 0:
+        if h - l == 1:
+            if _list[l] == search_value:
+                return l
+            elif _list[h] == search_value:
+                return h
+            else:
+                return -1
+        else:
+            mid = (l + h) // 2
+            if _list[mid] > search_value:
+                h = mid
+            else:
+                l = mid
+
+
+x = range(0, 100, 7)
+
+y = binary_search_recursive(list(x), 777, 0, len(x) - 1)
+z = binary_search_iterative(list(x), 777)
+
 print(list(x)[y])
+print(list(x)[z])
+
