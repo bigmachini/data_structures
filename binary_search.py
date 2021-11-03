da06615d6da9f2f9e665017f6b54e4ae3b63c6c3
ewaudo <ewaudo@copiakenya.com>
2021-11-03 13:18:26 +0300

    refactor code

--- binary_search.py
+++ binary_search.py
@@ -1,45 +1,36 @@
 def binary_search_recursive(_list, search_value, l, h):
-    if h - l == 1:
-        if _list[l] == search_value:
-            return l
-        elif _list[h] == search_value:
-            return h
-        else:
-            return -1
+    if l >= h:
+        return -1
     else:
         mid = (l + h) // 2
-        if _list[mid] > search_value:
-            h = mid
+        if _list[mid] == search_value:
+            return mid
+        elif _list[mid] > search_value:
+            h = mid - 1
         else:
-            l = mid
+            l = mid + 1
 
         return binary_search_recursive(_list, search_value, l, h)
 
 
 def binary_search_iterative(_list, search_value):
-    l = 0
-    h = len(_list)-1
-    while h - l > 0:
-        if h - l == 1:
-            if _list[l] == search_value:
-                return l
-            elif _list[h] == search_value:
-                return h
-            else:
-                return -1
+    l = 1
+    h = len(_list)
+    while l < h:
+        mid = (l + h) // 2
+        if _list[mid] == search_value:
+            return mid
+        elif _list[mid] > search_value:
+            h = mid - 1
         else:
-            mid = (l + h) // 2
-            if _list[mid] > search_value:
-                h = mid
-            else:
-                l = mid
+            l = mid + 1
+    return -1
 
 
 x = range(0, 100, 7)
 
-y = binary_search_recursive(list(x), 777, 0, len(x) - 1)
+y = binary_search_recursive(list(x), 777, 0, len(x))
 z = binary_search_iterative(list(x), 777)
 
 print(list(x)[y])
 print(list(x)[z])
-
